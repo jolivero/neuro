686b8573bebec28a2f751bcf794119d1033cc8ee
Daniel Rodas <36525134+daniir@users.noreply.github.com>
2025-06-21 09:09:45 -0500

    Nueva definición de Dtos

---
+++ Neuro.AI.Graph.Models/Dtos/User.Dto.cs
@@ -0,0 +1,17 @@
+namespace Neuro.AI.Graph.Models.Dtos
+{
+    public class UserIpcDto
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string DocumentId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string BloodType { get; set; } = string.Empty;
+        public string EmployeeNumber { get; set; } = string.Empty;
+        public string CompanyId { get; set; } = string.Empty;
+    }
+}
