2b661066a88e89cd2609675ae77808979bb129b0
Daniel Rodas <36525134+daniir@users.noreply.github.com>
2025-07-09 16:40:44 -0500

    Modelos de respuesta de base de datos

---
+++ Neuro.AI.Graph.Models/DbResponse/ProductionLine.cs
@@ -0,0 +1,27 @@
+namespace Neuro.AI.Graph.Models.DbResponse
+{
+    public class ProductionLineBasicInfo
+    {
+        public Guid LineId { get; set; }
+        public string LineName { get; set; } = string.Empty;
+        public string GroupName { get; set; } = string.Empty;
+        public string StationName { get; set; } = string.Empty;
+        public string MachineName { get; set; } = string.Empty;
+        public string PartName { get; set; } = string.Empty;
+        public string PreviousPartName { get; set; } = string.Empty;
+        public decimal RequiredQuantity { get; set; }
+        public Guid CreatedById { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class ProductionLineMachineHoursPerCut
+    {
+        public string GroupName { get; set; } = string.Empty;
+        public string StationName { get; set; } = string.Empty;
+        public string MachineName { get; set; } = string.Empty;
+        public int HoursPerCut { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+    }
+}
