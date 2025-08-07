bd15a0fbbb58ef6b9fc46a90fc8be6d653067853
Daniel Rodas <36525134+daniir@users.noreply.github.com>
2025-08-06 21:00:35 -0500

    Mutación de eliminación de paso en línea de producción

--- Neuro.AI.Graph.QL/Mutations/ManufacturingMutations.cs
+++ Neuro.AI.Graph.QL/Mutations/ManufacturingMutations.cs
@@ -50,9 +50,14 @@ namespace Neuro.AI.Graph.QL.Mutations
             return await repository.Create_productionLine_steps(plConfigDto);
         }
 
-        public async Task<string> repo_update_productionLine_steps(ProductionLineRepository repository, ProductionLineUpdateDto plUpdateDto)
+        public async Task<string> repo_update_productionLine_steps(ProductionLineRepository repository, ProductionLineHandleStepDto plUpdateDto)
         {
             return await repository.Update_productionLine_steps(plUpdateDto);
+        }
+
+        public async Task<string> repo_delete_productionLine_steps(ProductionLineRepository repository, ProductionLineHandleStepDto plDeleteDto)
+        {
+            return await repository.Delete_productionLine_steps(plDeleteDto);
         }
 
         #endregion
