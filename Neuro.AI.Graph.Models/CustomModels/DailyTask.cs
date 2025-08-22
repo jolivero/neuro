9c7f284fe7cd5aee6a51600fadade66e5ed34a4c
Daniel Rodas <36525134+daniir@users.noreply.github.com>
2025-08-22 12:26:16 -0500

    Actualización de Dto para tareas del operario

--- Neuro.AI.Graph.Models/CustomModels/DailyTask.cs
+++ Neuro.AI.Graph.Models/CustomModels/DailyTask.cs
@@ -6,6 +6,7 @@ namespace Neuro.AI.Graph.Models.CustomModels
     {
         public Guid TaskId { get; set; } = new();
         public string OperatorStatus { get; set; } = string.Empty;
+        public Progress Progress { get; set; } = new();
         public User User { get; set; } = new();
         public DailyTaskStation Station { get; set; } = new();
     }
@@ -17,6 +18,12 @@ namespace Neuro.AI.Graph.Models.CustomModels
         public Machine Machine { get; set; } = new();
         public Part Part { get; set; } = new();
         public List<Part> PrevPart { get; set; } = [];
+    }
+
+    public class Progress
+    {
+        public int Total { get; set; }
+        public decimal Compliance { get; set; }
     }
     
 }
