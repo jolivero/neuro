3e4171d4cf14f67a2e83a49bc6d551ae746df9b2
Daniel Rodas <36525134+daniir@users.noreply.github.com>
2025-09-26 15:17:27 -0500

    Actualización de modelo personalizado

--- Neuro.AI.Graph.Models/CustomModels/MonthlySchedule.cs
+++ Neuro.AI.Graph.Models/CustomModels/MonthlySchedule.cs
@@ -2,6 +2,26 @@ using Neuro.AI.Graph.Models.Manufacturing;
 
 namespace Neuro.AI.Graph.Models.CustomModels
 {
+
+    public class AnnualPlannigInfo
+    {
+        public int Year { get; set; }
+        public int Days { get; set; }
+        public int Tasks { get; set; }
+        public int Operators { get; set; }
+    }
+
+    public class MonthlyPlanningProgress
+    {
+        public string Operator { get; set; } = string.Empty;
+        public DateOnly ProductionDate { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public string Line { get; set; } = string.Empty;
+        public int Total { get; set; }
+        public decimal Progress { get; set; }
+    }
+
     public class MonthlyPlanningProductionLines : MonthlyPlanning
     {
         public List<AssignedProductionLines> AssignedProductionLines { get; set; } = [];
@@ -12,7 +32,7 @@ namespace Neuro.AI.Graph.Models.CustomModels
         public Guid LineId { get; set; }
         public string Name { get; set; } = string.Empty;
         public int MonthlyGoal { get; set; }
-        public decimal CurrentProgress { get; set; }
+        public int CurrentProgress { get; set; }
         public decimal Progress { get; set; }
     }
     
