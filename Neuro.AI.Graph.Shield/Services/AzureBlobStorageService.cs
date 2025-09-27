581514fc2a896a7a05ce29591bfadcf6f4a52818
Daniel Rodas <36525134+daniir@users.noreply.github.com>
2025-09-27 18:16:09 -0500

    Servicio para carga de logo en Azure Blob Storage

---
+++ Neuro.AI.Graph.Shield/Services/AzureBlobStorageService.cs
@@ -0,0 +1,31 @@
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Neuro.AI.Graph
+{
+    public class AzureBlobStorageService : IAzureBlobStorageService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly IConfiguration _config;
+
+        public AzureBlobStorageService(BlobServiceClient blobServiceClient, IConfiguration config)
+        {
+            _blobServiceClient = blobServiceClient;
+            _config = config;
+        }
+
+        public async Task<string> UploadFile(Stream fileStream, string fileName)
+        {
+            
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_config["AzureConnections:AzureContainerName"]);
+            await containerClient.SetAccessPolicyAsync(PublicAccessType.Blob);
+
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            await blobClient.UploadAsync(fileStream, overwrite: true);
+
+            return blobClient.Uri.ToString();
+        }
+    }
+}
