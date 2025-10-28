330f5044976fbe12de7ba708880fac5528648146
Daniel Rodas <36525134+daniir@users.noreply.github.com>
2025-10-28 08:58:35 -0500

    Corrección de tipo de dato

--- Neuro.AI.Graph.Models/CustomModels/ExtraTime.cs
+++ Neuro.AI.Graph.Models/CustomModels/ExtraTime.cs
@@ -3,6 +3,6 @@ namespace Neuro.AI.Graph.Models.CustomModels
     public class ExtraTimeResponse
     {
         public Guid UserId { get; set; }
-        public decimal ExtraTime { get; set; }
+        public TimeOnly ExtraTime { get; set; }
     }
 }
