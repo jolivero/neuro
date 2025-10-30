fc7dcb762c0309666e9ec43911193f8de0e5cfc1
Daniel Rodas <36525134+daniir@users.noreply.github.com>
2025-10-30 09:06:30 -0500

    Actualización de DbContext y nuevo modelo de datos agregado

---
+++ Neuro.AI.Graph.Models/Manufacturing/ChangePlanningDay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Neuro.AI.Graph.Models.Manufacturing;
+
+public partial class ChangePlanningDay
+{
+    public int ChangeDayId { get; set; }
+
+    public int? MonthId { get; set; }
+
+    public int? LineId { get; set; }
+
+    public int? DayId { get; set; }
+
+    public int? DailyGoal { get; set; }
+
+    public DateOnly? ProductionDate { get; set; }
+
+    public string? DayType { get; set; }
+
+    public int? Available { get; set; }
+
+    public int? RequestId { get; set; }
+
+    public virtual ProductionChangeRequest? Request { get; set; }
+}
